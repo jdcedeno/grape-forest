ce835a10dfbe8eca90b179b881cff48bfa929595
Jose Cedeno <56703919+jdcedeno@users.noreply.github.com>
2021-04-17 16:15:39 -0400

    leetcode findtownjudge: createDirectedGraph function works

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const { unDirUnW, unDirWei, dirUnw, dirWei } = require("./examples");
 const makeGraph = require("./graphs");
 
-console.log("unDirUnW: ", unDirUnW);
-console.log("unDirWei: ", unDirWei);
-console.log("dirUnw: ", dirUnw);
-console.log("dirWei: ", dirWei);
-
 let adjList1 = makeGraph(unDirUnW);
 console.log(adjList1);
+
 let adjList2 = makeGraph(unDirWei);
 console.log(adjList2);
+
 let adjList3 = makeGraph(dirUnw);
 console.log(adjList3);
+
 let adjList4 = makeGraph(dirWei);
 console.log(adjList4);
